84529835d4b56ebf29d7b01b547a4925685faf7b
marwaronron <marwa.siala2017@gmail.com>
2018-11-12 23:49:13 +0100

    imc Interface

---
+++ app/src/main/java/com/example/siwar/fitwoman/IMC.java
@@ -0,0 +1,13 @@
+package com.example.siwar.fitwoman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
