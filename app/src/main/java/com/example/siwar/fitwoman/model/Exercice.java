bc31531a8cd48c9ba17dfa8ee3158fd9553f1e66
marwaronron <marwa.siala2017@gmail.com>
2018-11-26 22:00:55 +0100

    hello123

---
+++ app/src/main/java/com/example/siwar/fitwoman/model/Exercice.java
@@ -0,0 +1,35 @@
+package com.example.siwar.fitwoman.model;
+
+public class Exercice {
+    public String name ,description,image ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Exercice(String name, String description, String image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
